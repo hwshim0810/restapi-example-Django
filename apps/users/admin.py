7573c9dee515a9ebfc1ea79f9b172f44040a3c08
hyunwoo.shim <shim.hw0810@gmail.com>
2018-08-01 23:06:05 +0900

    Add user admin page

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'email',
+        'name',
+        'is_staff',
+        'is_superuser',
+        'created_at',
+    )
+    list_display_links = (
+        'id',
+        'email',
+        'name',
+    )
+    list_filter = (
+        'is_staff',
+        'is_superuser',
+        'created_at',
+    )
+    fieldsets = (
+        (None, {
+            'fields': (
+                'email',
+                'name',
+                'job',
+            )
+        }),
+        ('Permissions', {
+            'fields': (
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+        ('Dates', {
+            'fields': (
+                'created_at',
+            )
+        })
+    )
+    search_fields = (
+        'email',
+        'name',
+    )
+    readonly_fields = (
+        'email',
+        'created_at',
+    )

ab4f78d906fd15f4d6db6e736aac929a71d81a34
hyunwoo.shim <shim.hw0810@gmail.com>
2018-08-01 21:57:00 +0900

    Add allauth, restframework, rest_auth

--- restexample/settings/base.py
+++ restexample/settings/base.py
@@ -56,6 +56,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
 
+    # ## Third Party apps
+    'allauth',
+    'allauth.account',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'rest_auth.registration',
+
     # ## Project apps ##
 ]
 
@@ -134,3 +143,17 @@ USE_TZ = False
 # ##### EMAIL CONFIGURATION ######################
 
 EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+
+# ### ALL AUTH SETTINGS ###
+
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+
+ACCOUNT_EMAIL_REQUIRED = True
+
+ACCOUNT_UNIQUE_EMAIL = True
+
+ACCOUNT_USERNAME_REQUIRED = False
+
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'

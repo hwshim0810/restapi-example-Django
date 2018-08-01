b361b26b02d347e979fbf8b726ecec710c331e3d
hyunwoo.shim <shim.hw0810@gmail.com>
2018-08-01 23:13:56 +0900

    Fix wrong name

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from rest_auth.registration import views as registeration_views
+from rest_auth.registration import views as registration_views
 
 urlpatterns = [
 
     # Registration
-    path('', registeration_views.RegisterView.as_view(), name='registration'),
+    path('', registration_views.RegisterView.as_view(), name='registration'),
 ]

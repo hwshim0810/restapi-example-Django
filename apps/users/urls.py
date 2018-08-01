604f4199a104229fbd116b0a4b09dcbe5185ac93
hyunwoo.shim <shim.hw0810@gmail.com>
2018-08-02 00:31:07 +0900

    Remove url slash

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
 
     # Registration
-    path('/', registration_views.RegisterView.as_view(), name='registration'),
+    path('', registration_views.RegisterView.as_view(), name='registration'),
 
 ]

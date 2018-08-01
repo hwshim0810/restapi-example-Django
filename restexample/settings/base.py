7738d2e9029fcb76d371948d95650dbea9f5cf5b
hyunwoo.shim <shim.hw0810@gmail.com>
2018-08-02 02:32:55 +0900

    Add cache setting

--- restexample/settings/base.py
+++ restexample/settings/base.py
@@ -172,3 +172,14 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
 }
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}

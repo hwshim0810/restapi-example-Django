8b58071d021001c6be410415246d2a4b44cc5176
hyunwoo.shim <shim.hw0810@gmail.com>
2018-08-01 23:43:16 +0900

    Add login view

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 from rest_auth.registration import views as registration_views
+from rest_auth import views as auth_views
 
 urlpatterns = [
+
+    # Login
+    path('login', auth_views.LoginView.as_view(), name='login'),
 
     # Registration
     path('', registration_views.RegisterView.as_view(), name='registration'),

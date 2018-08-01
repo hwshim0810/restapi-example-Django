23fa5c8e2fc99411a7f45bebb7b892a513cec89f
hyunwoo.shim <shim.hw0810@gmail.com>
2018-08-02 02:01:19 +0900

    Add feeds app models

--- apps/feeds/models.py
+++ apps/feeds/models.py
@@ -1,1 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+from polymorphic.models import PolymorphicModel
+
+from utility.models import UpdatedAtModel
+
+
+class Content(PolymorphicModel, UpdatedAtModel):
+
+    user = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
+
+
+class Post(Content):
+
+    title = models.CharField(max_length=50)
+    text = models.TextField(max_length=3000)
+
+
+class Image(Content):
+
+    path = models.CharField(max_length=200)
+    caption = models.TextField(max_length=3000, blank=True)
+
